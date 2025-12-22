6012cfeca81e5c02faeccf14e99d82dde0349022
WangY18 <1165768961@qq.com>
2025-12-22 11:17:56 +0800

    Move Files

---
+++ examples/pure/demo_Raster.cpp
@@ -0,0 +1,40 @@
+#include "NEPathPlanner.h"
+#include "FileAgent.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main()
+{
+    NEPathPlanner planner;
+
+    // Set the contour
+    path contour;
+    contour.length = 1000;                    // the number of waypoints
+    contour.x = new double[contour.length](); // x-coordinate of waypoints
+    contour.y = new double[contour.length](); // y-coordinate of waypoints
+    const double pi = acos(-1.0);             // pi == 3.1415926...
+    for (int i = 0; i < contour.length; ++i)
+    {
+        double theta = 2.0 * pi * i / contour.length;
+        double r = 15.0 * (1.0 + 0.15 * cos(10.0 * theta));
+        contour.x[i] = r * cos(theta);
+        contour.y[i] = r * sin(theta);
+    }
+    planner.set_contour(contour);
+    // or `planner.set_contour(contour.x, contour.y, contour.length)`
+
+    // Set the toolpath parameters
+    DirectParallelOptions opts;
+    opts.delta = 1.0;       // the line width of toolpaths
+    opts.angle = -pi / 3.0; // the angle of raster toolpaths, unit: rad
+
+    paths raster_paths = planner.Raster(opts); // all raster paths
+    cout << "There are " << raster_paths.size() << " continuous toolpaths in total." << endl;
+
+    FileAgent::delete_AllFiles((fs::path(__FILE__).parent_path() / "data_examples" / "demo_raster").string().c_str());
+    FileAgent::write_csv(raster_paths, (fs::path(__FILE__).parent_path() / "data_examples" / "demo_raster").string().c_str(), ".csv");
+    // FileAgent::write_csv(contour, R"(.\data_examples\contour.csv)");
+}
