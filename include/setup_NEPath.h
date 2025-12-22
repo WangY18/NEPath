59404aab090a493e492a8a352b4e0faab23b52b5
WangY18 <1165768961@qq.com>
2025-12-22 14:51:11 +0800

    set ipopt try

--- include/setup_NEPath.h
+++ include/setup_NEPath.h
@@ -1,8 +1,8 @@
 // Conditionally enable IPOPT support (open-source optimization solver)
 // IPOPT replaces Gurobi for non-equidistant toolpath optimization (IQOP)
-// #ifndef IncludeIpopt
-// #define IncludeIpopt 1
-// #endif
+#ifndef IncludeIpopt
+#define IncludeIpopt 1
+#endif
 
 // Legacy Gurobi support (deprecated, use IPOPT instead)
 // #ifndef IncludeGurobi
